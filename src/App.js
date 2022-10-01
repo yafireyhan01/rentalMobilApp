d176e1e16cf73877b59109aaaea5777d1fa8a7c6
yafireyhan01 <yafireyhan01@gmail.com>
2022-10-01 11:53:33 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
+import image from './src/image/onboarding.jpg';
+
+const App = () => {
+  return (
+    <View>
+      <ImageBackground source={image} style={styles.imageBackground}>
+        <Text style={styles.title}>Temukan dan sewa mobil dengan mudah</Text>
+        <TouchableOpacity style={styles.button.container}>
+          <Text style={styles.button.text}>Mulai Sekarang</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  title: {
+    color: '#ffff',
+    fontSize: 50,
+    marginTop: 290,
+    maxWidth: 300,
+    marginLeft: 110,
+    fontWeight: 'bold',
+  },
+  imageBackground: {
+    width: 638,
+    height: 1050,
+    left: -95,
+    top: -200,
+  },
+  button: {
+    container: {
+      backgroundColor: 'black',
+      padding: 20,
+      borderRadius: 25,
+      maxWidth: 200,
+      left: 150,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 200,
+      marginLeft: 25,
+    },
+    text: {
+      color: 'white',
+      fontWeight: 'bold',
+    },
+  },
+});
