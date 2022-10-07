48fe71c9f144f2dda605437de636ceefcf15884b
yafireyhan01 <yafireyhan01@gmail.com>
2022-10-07 09:19:11 +0700

    Car image added

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,24 @@
 import ChatIcon from './icons/chatIcon.svg';
 import LocationIcon from './icons/locationIcon.svg';
 
+//Logos
+import BmwLogo from './logos/bmw.svg';
+import DaihatsuLogo from './logos/daihatsu.svg';
+import MitsubishiLogo from './logos/mitsubishi.svg';
+import ToyotaLogo from './logos/toyota.svg';
 //Image
 import CarBackground from './images/onboarding.jpg';
+import Xenia from './images/daihatsu-xenia.svg';
+import Expander from './images/mitsubishi-xpander.svg';
 
-export {ChatIcon, CarBackground, LocationIcon};
+export {
+  ChatIcon,
+  CarBackground,
+  LocationIcon,
+  BmwLogo,
+  DaihatsuLogo,
+  MitsubishiLogo,
+  ToyotaLogo,
+  Xenia,
+  Expander,
+};
