be3eff7f60514db37706a3eb8666a1a84ff9007e
yafireyhan01 <yafireyhan01@gmail.com>
2022-10-07 15:54:21 +0700

    Add home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import {
   Image,
   ImageBackground,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
@@ -8,17 +9,223 @@ import {
   View,
 } from 'react-native';
 import React from 'react';
-import {ChatIcon, LocationIcon} from '../../assets';
+import {
+  ChatIcon,
+  LocationIcon,
+  BmwLogo,
+  DaihatsuLogo,
+  MitsubishiLogo,
+  ToyotaLogo,
+  Xenia,
+  YellowStar,
+  Expander,
+} from '../../assets';
+
+const CarLogo = props => {
+  return (
+    <View>
+      <TouchableOpacity>
+        <Image source={props.BmwLogo} />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const Home = ({navigation}) => {
   return (
     <View>
-      <View style={styles.container.top}>
-        <LocationIcon style={styles.icon.location} />
+      <View style={styles.container.top.container}>
+        <View style={styles.container.top.textContainer}>
+          <LocationIcon />
+          <View style={{marginLeft: 17}}>
+            <Text style={{color: '#505050'}}>Lokasi Anda</Text>
+            <Text style={{color: 'black', fontWeight: 'bold'}}>
+              Bojongsari, Depok
+            </Text>
+          </View>
+          <View style={styles.container.top.photo.container}>
+            <View style={styles.container.top.photo.dimension} />
+          </View>
+        </View>
+        <Text style={styles.container.top.title}>
+          Temukan Kendaraan Terbaikmu
+        </Text>
       </View>
       <View style={styles.container.bottom}>
-        <TextInput style={styles.textInput} />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Cari Kendaraan Terbaikmu..."
+          placeholderTextColor="#505050"
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            maxWidth: 400,
+            marginTop: -10,
+          }}>
+          <Text
+            style={{
+              color: 'black',
+              marginTop: 26,
+              marginLeft: 22,
+              fontSize: 15,
+              fontWeight: 'bold',
+            }}>
+            Paling dicari
+          </Text>
+          <TouchableOpacity>
+            <Text
+              style={{
+                color: '#5a5a5a',
+                marginTop: 26,
+                marginRight: 22,
+                fontSize: 12,
+              }}>
+              Lihat Semua
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <ScrollView horizontal style={{maxHeight: 80}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginTop: 24,
+              marginLeft: 22,
+            }}>
+            <TouchableOpacity style={{marginRight: 40}}>
+              <DaihatsuLogo />
+            </TouchableOpacity>
+            <TouchableOpacity style={{marginRight: 40}}>
+              <ToyotaLogo />
+            </TouchableOpacity>
+            <TouchableOpacity style={{marginRight: 40}}>
+              <MitsubishiLogo />
+            </TouchableOpacity>
+            <TouchableOpacity style={{marginRight: 40}}>
+              <BmwLogo />
+            </TouchableOpacity>
+            {/* <CarLogo image={BmwLogo} /> */}
+          </View>
+        </ScrollView>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            maxWidth: 400,
+          }}>
+          <Text
+            style={{
+              color: 'black',
+              marginTop: 26,
+              marginLeft: 22,
+              fontSize: 15,
+              fontWeight: 'bold',
+            }}>
+            Tersedia Didekatmu
+          </Text>
+          <TouchableOpacity>
+            <Text
+              style={{
+                color: '#5a5a5a',
+                marginTop: 26,
+                marginRight: 22,
+                fontSize: 12,
+              }}>
+              Lihat Semua
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <ScrollView horizontal style={{maxHeight: 200}}>
+          <View>
+            <View style={{flexDirection: 'row'}}>
+              <View style={{marginHorizontal: 23}}>
+                <TouchableOpacity>
+                  <Xenia style={{marginTop: 20}} />
+                  <Text
+                    style={{
+                      color: 'black',
+                      marginTop: 6,
+                      fontWeight: 'bold',
+                    }}>
+                    Daihatsu Xenia 2020
+                  </Text>
+                  <View style={{flexDirection: 'row'}}>
+                    <YellowStar style={{marginTop: 11}} />
+                    <Text
+                      style={{
+                        marginTop: 7,
+                        marginLeft: 5,
+                        color: 'black',
+                        fontWeight: 'bold',
+                      }}>
+                      4.7
+                    </Text>
+                    <Text style={{marginTop: 7, color: '#686666'}}>
+                      (14 review)
+                    </Text>
+                    <Text
+                      style={{
+                        marginTop: 7,
+                        color: 'black',
+                        marginRight: 5,
+                        marginLeft: 20,
+                      }}>
+                      300.000/Hari
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+              <View style={{marginLeft: 28}}>
+                <TouchableOpacity>
+                  <Expander style={{marginTop: 20, height: 45, width: 45}} />
+                  <Text
+                    style={{
+                      color: 'black',
+                      marginTop: 6,
+                      fontWeight: 'bold',
+                    }}>
+                    Mitsubishi Expander 2022
+                  </Text>
+                  <View style={{flexDirection: 'row'}}>
+                    <YellowStar style={{marginTop: 11}} />
+                    <Text
+                      style={{
+                        marginTop: 7,
+                        marginLeft: 5,
+                        color: 'black',
+                        fontWeight: 'bold',
+                      }}>
+                      4.7
+                    </Text>
+                    <Text style={{marginTop: 7, color: '#686666'}}>
+                      (14 review)
+                    </Text>
+                    <Text
+                      style={{
+                        marginTop: 7,
+                        color: 'black',
+                        marginRight: 5,
+                        marginLeft: 20,
+                      }}>
+                      300.000/Hari
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
       </View>
+      <View
+        style={{
+          backgroundColor: 'white',
+          borderColor: 'black',
+          borderWidth: 1,
+          height: 70,
+          borderRadius: 25,
+          marginTop: -70,
+        }}></View>
     </View>
   );
 };
@@ -32,17 +239,46 @@ const styles = StyleSheet.create({
     marginTop: -25,
     borderColor: 'white',
     borderWidth: 2,
+    paddingLeft: 24,
     elevation: 8,
     borderRadius: 25,
-  },
-  icon: {
-    location: {
-      marginLeft: 22,
-      marginTop: 81,
-    },
+    color: 'black',
   },
   container: {
-    top: {backgroundColor: '#C1BEBE', height: 304},
+    top: {
+      container: {
+        backgroundColor: '#C1BEBE',
+        height: 304,
+      },
+      textContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 22,
+        marginTop: 50,
+      },
+      photo: {
+        container: {
+          flex: 1,
+          justifyContent: 'flex-end',
+          alignItems: 'flex-end',
+          marginRight: 26,
+        },
+        dimension: {
+          borderRadius: 25,
+          height: 45,
+          width: 45,
+          backgroundColor: 'black',
+        },
+      },
+      title: {
+        color: 'black',
+        marginLeft: 22,
+        marginTop: 50,
+        fontSize: 20,
+        maxWidth: 202,
+        fontWeight: 'bold',
+      },
+    },
     bottom: {
       backgroundColor: 'white',
       height: 480,
