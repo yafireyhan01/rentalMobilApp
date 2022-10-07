b1dbce18136d17edca472e672842c04d7f81f8e7
yafireyhan01 <yafireyhan01@gmail.com>
2022-10-07 15:54:07 +0700

    More icon added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,12 @@
 //Icon
 import ChatIcon from './icons/chatIcon.svg';
 import LocationIcon from './icons/locationIcon.svg';
-
+import YellowStar from './icons/yellow_star.svg';
+import ActivityIcon from './icons/Activity.svg';
+import BellIcon from './icons/Bell.svg';
+import DownIcon from './icons/downIcon.svg';
+import HeartIcon from './icons/Heart.svg';
+import PersonIcon from './icons/Person.svg';
 //Logos
 import BmwLogo from './logos/bmw.svg';
 import DaihatsuLogo from './logos/daihatsu.svg';
@@ -22,4 +27,10 @@ export {
   ToyotaLogo,
   Xenia,
   Expander,
+  YellowStar,
+  ActivityIcon,
+  BellIcon,
+  DownIcon,
+  HeartIcon,
+  PersonIcon,
 };
